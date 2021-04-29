14e15a33b8c13fec8f912f0022009d8429963a90
Lucas Barra <lucastbarra@hotmail.com>
2021-04-29 17:23:43 -0300

    cirando o tabuleiro

--- src/MemoryGame.js
+++ src/MemoryGame.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import GameBoard from './components/GameBoard';
 import GameOver from './components/GameOver';
+import game from './game';
 
-export default function MemoryGame(){
-
+export default function MemoryGame() {
 
     const [gameOver, setGameOver] = useState(false);
+    const [cards, setCards] = useState([]);
 
-    function restart(){
+    useEffect(() => {
+        setCards(game.createCardsFromTechs());
+    }, []);
+
+
+    function restart() {
         setGameOver(false);
     }
 
     return (
         <div>
+            <GameBoard cards={cards} />
             <GameOver show={gameOver} handleRestart={restart} />
         </div>
     );
